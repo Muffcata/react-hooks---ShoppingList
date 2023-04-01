69621fc8fa429b1638f907ac92292eb3a0cff8e9
Muffcata <marta.oched@gmail.com>
2023-04-01 20:43:16 +0200

    hooks added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
-import { Component } from 'react';
 import ShoppingList from './ShoppingList/ShoppingList';
 
-export class App extends Component {
-  handleLogin = state => {
-    console.log(state);
-  };
-  render() {
-    return (
-      <div>
-        <ShoppingList />
-      </div>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <div>
+      <ShoppingList />
+    </div>
+  );
+};
 
 export default App;
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,7 +1,9 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { ShoppingItem } from 'components/ShoppingItem/ShoppingItem';
 import style from './ShoppingList.module.css';
 
+import React from 'react';
+import { useEffect, useState } from 'react';
 //componentDidMount -
 //we take with localStorage last saved state
 // componentDidUpdate -
@@ -10,85 +12,99 @@ import style from './ShoppingList.module.css';
 // componentWillUnmount -
 //   alert informating about removing with list
 
-export class ShoppingList extends Component {
-  state = {
-    items: [],
-    newItemName: '',
+// export class ShoppingList extends Component {
+//   state = {
+//     items: [],
+//     newItemName: '',
+//   };
+
+export const ShoppingList = () => {
+  const [items, setItems] = useState([]);
+  const [newItemName, setNewItemName] = useState('');
+
+  const deleteItems = id => {
+    const newItemState = items.filter(item => item.id !== id);
+    setItems(newItemState);
   };
 
-  deleteItems = id => {
-    this.setState(state => ({
-      items: state.items.filter(item => item.id !== id),
-    }));
-  };
-
-  addItem = evt => {
+  const addItem = evt => {
     evt.preventDefault();
-
-    const { items } = this.state;
     const lastId =
       items.length > 0 ? Math.max(...items.map(item => item.id)) : 0;
-    this.setState(state => ({
-      items: state.items.concat({ name: state.newItemName, id: lastId + 1 }),
-      newItemName: '',
-    }));
+    const newItemsState = items.concat({ name: newItemName, id: lastId + 1 });
+    setItems(newItemsState);
+    setNewItemName('');
   };
 
-  handleInputChange = evt => {
-    this.setState(state => ({ newItemName: evt.target.value }));
-  };
+  // handleInputChange = evt => {
+  //   this.setState(state => ({ newItemName: evt.target.value }));
+  // };
+
+  const handleInputChange = evt => setNewItemName(evt.target.value);
+
   //method//
   //   shouldComponentUpdate(nextProps, nextState) {
   //     if (nextProps.value > 100) {
   //       return; // props from parents >100 is not render
   //     }
   //   }
-  componentDidMount() {
+  //   componentDidMount() {
+  //     const list = window.localStorage.getItem('shoppin-list');
+  //     console.log('list');
+  //     if (!list) return; //if list no exist return nothing
+  //     try {
+  //       this.setState({
+  //         items: JSON.parse(list),
+  //       });
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  // }
+
+  useEffect(() => {
     const list = window.localStorage.getItem('shoppin-list');
     console.log('list');
     if (!list) return; //if list no exist return nothing
     try {
-      this.setState({
-        items: JSON.parse(list),
-      });
+      setItems(JSON.parse(list));
     } catch (e) {
       console.error(e);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.items.length !== this.state.length) {
-      const shoppingListStringified = JSON.stringify(this.state.items);
-      window.localStorage.setItem('shoppin-list', shoppingListStringified);
-    }
-  }
-  render() {
-    return (
-      <div className={style.list}>
-        <h1>Add ingredients:</h1>
-        <form className={style.form} onSubmit={this.addItem}>
-          <input
-            className={style.input}
-            value={this.state.newItemName}
-            type="text"
-            onChange={this.handleInputChange}
-          />
-          <button className={style.button} type="submit">
-            Add to list
-          </button>
-        </form>
-        <h2> List:</h2>
-        {this.state.items.map((item, index) => {
-          return (
-            <ShoppingItem
-              item={item}
-              deleteItems={this.deleteItems}
-              key={index}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    const shoppingListStringified = JSON.stringify(items);
+    window.localStorage.setItem('shoppin-list', shoppingListStringified);
+  }, [items]);
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.items.length !== this.state.length) {
+  //     const shoppingListStringified = JSON.stringify(this.state.items);
+  //     window.localStorage.setItem('shoppin-list', shoppingListStringified);
+  //   }
+  // }
+
+  return (
+    <div className={style.list}>
+      <h1>Add ingredients:</h1>
+      <form className={style.form} onSubmit={addItem}>
+        <input
+          className={style.input}
+          value={newItemName}
+          type="text"
+          onChange={handleInputChange}
+        />
+        <button className={style.button} type="submit">
+          Add to list
+        </button>
+      </form>
+      <h2> List:</h2>
+      {items.map((item, index) => {
+        return (
+          <ShoppingItem item={item} deleteItems={deleteItems} key={index} />
+        );
+      })}
+    </div>
+  );
+};
+
 export default ShoppingList;
